5f48b30a8894334f048ba8fd459d2773883df9c0
Huỳnh Phúc Toàn <97122589+WizardSC@users.noreply.github.com>
2023-06-21 21:12:52 +0700

    update

---
+++ src/Controller/DashboardController.java
@@ -0,0 +1,18 @@
+package Controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.HBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DashboardController implements Initializable {
+    @FXML
+    private HBox cardLayout;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
