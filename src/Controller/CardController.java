3de078df7a4ccc8f42957d2e95f2e0723607b6bd
Huỳnh Phúc Toàn <97122589+WizardSC@users.noreply.github.com>
2023-06-21 22:06:45 +0700

    done dynamic hbox

--- src/Controller/CardController.java
+++ src/Controller/CardController.java
@@ -1,5 +1,35 @@
 package Controller;
 
+import Model.Book;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+
 public class CardController {
 
+    @FXML
+    private Label authorName;
+
+    @FXML
+    private Label bookName;
+
+    @FXML
+    private HBox box;
+
+    @FXML
+    private ImageView bookImage;
+
+    private String [] colors = {"B9E5FF", "BDB2FE", "FB9AA8", "FF5056"};
+
+    public void setData(Book book){
+        Image image = new Image(getClass().getResourceAsStream(book.getImageSrc()));
+        bookImage.setImage(image);
+        bookName.setText(book.getName());
+        authorName.setText(book.getAuthor());
+        box.setStyle("-fx-background-color: #"+ colors[(int)(Math.random()* colors.length)]);
+
+    }
 }
--- src/Controller/DashboardController.java
+++ src/Controller/DashboardController.java
@@ -1,18 +1,68 @@
 package Controller;
 
+import Model.Book;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.HBox;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class DashboardController implements Initializable {
     @FXML
     private HBox cardLayout;
 
+    private ArrayList<Book> recentlyAdded;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        recentlyAdded = new ArrayList<>(recentlyAdded());
+        try {
+            for (int i = 0; i < recentlyAdded.size(); i++) {
+                FXMLLoader fxmlLoader = new FXMLLoader();
+                fxmlLoader.setLocation(getClass().getResource("/View/card.fxml"));
 
+
+                HBox cardBox = fxmlLoader.load();
+                CardController cardController = fxmlLoader.getController();
+                cardController.setData(recentlyAdded.get(i));
+                cardLayout.getChildren().add(cardBox);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private ArrayList<Book> recentlyAdded() {
+        ArrayList<Book> dsRecentlyAdded = new ArrayList<>();
+        Book book = new Book();
+        book.setName("CÂY CHUỐI NON ĐI\nGIÀY XANH");
+        book.setAuthor("Nguyễn Nhật Ánh");
+        book.setImageSrc("/img/caychuoinondigiayxanh.jpg");
+
+        dsRecentlyAdded.add(book);
+
+        book = new Book();
+        book.setName("BẢY BƯỚC TỚI \nMÙA HÈ");
+        book.setAuthor("Nguyễn Nhật Ánh");
+        book.setImageSrc("/img/baybuoctoimuahe.jpg");
+
+        dsRecentlyAdded.add(book);
+        book = new Book();
+        book.setName("NGỒI KHÓC TRÊN CÂY");
+        book.setAuthor("Nguyễn Nhật Ánh");
+        book.setImageSrc("/img/ngoikhoctrencay.jpg");
+
+        dsRecentlyAdded.add(book);
+        book = new Book();
+        book.setName("NGÀY XƯA CÓ MỘT\nCHUYỆN TÌNH");
+        book.setAuthor("Nguyễn Nhật Ánh");
+        book.setImageSrc("/img/ngayxuacomotchuyentinh.jpg");
+
+        dsRecentlyAdded.add(book);
+        return dsRecentlyAdded;
     }
 }
